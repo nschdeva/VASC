cd8eb3d423f6e381b473273445b334cceb1477cc
Aishwarya Solanki <pennywise97@Aishwaryas-MacBook-Pro.local>
2023-11-25 14:45:08 -0500

    update mypy version

--- src/CipherScan.py
+++ src/CipherScan.py
@@ -7,7 +7,7 @@ try:
 except:
 	print('\nFile WeakCiphers.csv can\'t be opened\n')
 else:
-	givenwclist = np.unique(list(givenwclist.iloc[:,0]))
+	givenwclist = lu.unique(list(givenwclist.iloc[:,0]))
 
 
 siteciphersSSLv2 = pd.DataFrame(columns = ['Website', 'IP']).set_index('Website')
